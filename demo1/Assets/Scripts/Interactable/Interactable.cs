e0656bec2a78a3a6a0df0fdc75abf3d403c80d1e
iann <ianathanwong@gmail.com>
2026-01-08 14:45:29 -0800

    Interactable Parent Class

--- demo1/Assets/Scripts/Interactable/Interactable.cs
+++ demo1/Assets/Scripts/Interactable/Interactable.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string promptMessage = "Interact (E)";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void Interact() => Interaction();
+    protected virtual void Interaction() {}
 }
