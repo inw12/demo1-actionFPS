b09621225c6244ae2ed0c1b023173a479fd9482c
iann <ianathanwong@gmail.com>
2026-01-08 14:51:40 -0800

    Raycast Setup
    Player now draws a raycast out of camera to check for interactable objects

--- demo1/Assets/Scripts/Player/PlayerInteract.cs
+++ demo1/Assets/Scripts/Player/PlayerInteract.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
-
 public class PlayerInteract : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Raycast Lengths")]
+    [SerializeField] private float shortRayDistance;    // objects up close
+    [SerializeField] private float farRayDistance;      // objects far away
+    [SerializeField] private LayerMask mask;            // interaction mask
+    private Camera cam;
 
-    // Update is called once per frame
-    void Update()
+    private void Start() {
+        cam = GetComponentInChildren<Camera>();
+    }
+    private void Update()
     {
-        
+        Ray ray = new(cam.transform.position, cam.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * shortRayDistance);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, shortRayDistance, mask))
+        {
+            
+        }
     }
 }
