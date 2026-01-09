09294f03eeddb042c538beaa2199c7d284aaaed1
iann <ianathanwong@gmail.com>
2026-01-09 12:05:39 -0800

    Updated GrapplePointInteractable script

--- demo1/Assets/Scripts/Interactable/Types/GrapplePointInteractable.cs
+++ demo1/Assets/Scripts/Interactable/Types/GrapplePointInteractable.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 public class GrapplePointInteractable : Interactable
 {
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private float timeToTarget;
+
     protected override void Interaction()
     {
-        Debug.Log("Grapple Hook Triggered!");
+        
     }
 }
