01d60ffe45263f0ef5d7a1216ac74cc5830f99af
iann <ianathanwong@gmail.com>
2026-01-08 18:03:19 -0800

    Interactable Scanning
    • Player can now scan for interactable objects that are a given distance away

--- demo1/Assets/Scripts/Player/PlayerInteract.cs
+++ demo1/Assets/Scripts/Player/PlayerInteract.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class PlayerInteract : MonoBehaviour
 {
     [Header("Raycast Lengths")]
-    [SerializeField] private float shortRayDistance;    // objects up close
-    [SerializeField] private float farRayDistance;      // objects far away
-    [SerializeField] private LayerMask mask;            // interaction mask
+    [SerializeField] private float closeInteractionRange;    
+    [SerializeField] private float farInteractionRange;     
+    [SerializeField] [Range(0, 1)] private float visionRadius;
+    [SerializeField] private LayerMask mask;            
     [Header("HUD Elements")]
     [SerializeField] private TextMeshProUGUI promptText;
     private Camera cam;
@@ -15,12 +16,50 @@ public class PlayerInteract : MonoBehaviour
     }
     private void Update()
     {
-        promptText.text = string.Empty;
-
+        UpdateHUD(string.Empty);
+        ScanForProximity();
+        ScanForVision();
+    }
+    private void ScanForProximity()
+    {
         Ray ray = new(cam.transform.position, cam.transform.forward);
-        Debug.DrawRay(ray.origin, ray.direction * shortRayDistance);
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, shortRayDistance, mask)) {
-            promptText.text = hitInfo.collider.GetComponent<Interactable>().promptMessage;
+        Debug.DrawRay(ray.origin, ray.direction * closeInteractionRange);
+        if (Physics.Raycast(ray, out RaycastHit hit, closeInteractionRange, mask) && hit.collider.TryGetComponent(out Interactable interactable)) {
+            UpdateHUD(interactable.promptMessage);
         }
     }
+    private void ScanForVision()
+    {
+        // ALL collisions detected by sphere
+        Collider[] hits = Physics.OverlapSphere(
+            cam.transform.position,
+            farInteractionRange,
+            mask
+        );
+
+        // Filter through every collision detected within the sphere
+        float bestDot = -1f;
+        foreach (var hit in hits)
+        {
+            // ignore if object is not 'Interactable'
+            if (!hit.TryGetComponent(out Interactable interactable)) {
+                continue;
+            }
+            if (interactable.interactionType != Interactable.InteractionType.Vision) {
+                continue;
+            }
+            
+            // draw vision cone
+            Vector3 targetDirection = (hit.transform.position - cam.transform.position).normalized;
+            float dot = Vector3.Dot(cam.transform.forward, targetDirection);
+
+            // if object is within ideal range
+            if (dot > visionRadius && dot > bestDot)
+            {
+                bestDot = dot;
+                UpdateHUD(interactable.promptMessage);
+            }
+        }
+    }
+    private void UpdateHUD(string text) => promptText.text = text;
 }
