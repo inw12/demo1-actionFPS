04fcb163e8a691eaa49e77aef14f5ab35dda7fad
iann <ianathanwong@gmail.com>
2026-01-08 17:18:30 -0800

    Vision Interactable Setup

--- demo1/Assets/Scripts/Interactable/Interactable.cs
+++ demo1/Assets/Scripts/Interactable/Interactable.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
-
 public class Interactable : MonoBehaviour
 {
+    public enum InteractionType {
+        Proximity, Vision
+    }
+    public InteractionType interactionType;
     public string promptMessage = "Interact (E)";
 
     public void Interact() => Interaction();
---
+++ demo1/Assets/Scripts/Interactable/VisionInteractableTest.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public class VisionInteractableTest : Interactable
+{
+    protected override void Interaction()
+    {
+        Debug.Log("Vision Interaction Works!");
+    }
+}
