95b3c4ad68b34dc82f6c03834696ddd84ae57acb
iann <ianathanwong@gmail.com>
2026-01-11 13:23:03 -0800

    On-Hit Emission
    Enemies briefly light up when hit

--- demo1/Assets/Scripts/EnemyHealth.cs
+++ demo1/Assets/Scripts/EnemyHealth.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
+    public Color hitColor;
+    public float lerpSpeed;
     [SerializeField] private float health;
     private float currentHealth;
+    private Material material;
+    private Color defaultColor;
 
-    private void Start() => currentHealth = health;
+    private void Start() {
+        currentHealth = health;
+        material = GetComponent<MeshRenderer>().material;
+        material.EnableKeyword("_EMISSION");
+        defaultColor = new(0, 0, 0);
+    }
+    private void Update()
+    {
+        Color current = material.GetColor("_EmissionColor");
+        Color next = Color.Lerp(current, defaultColor, Time.deltaTime * lerpSpeed);
+        material.SetColor("_EmissionColor", next);
+    }
     public void Damage(float amount) {
-        Debug.Log("Hit!");
         currentHealth -= amount;
+        material.SetColor("_EmissionColor", hitColor * 5f);
         CheckForDeath();
     }
     private void CheckForDeath()
