3525d3af44b9e68f79c960f5029074c06d817960
iann <ianathanwong@gmail.com>
2026-01-11 13:30:57 -0800

    Pulse Effect
    Enemies' models now pulse a bit when hit

--- demo1/Assets/Scripts/EnemyHealth.cs
+++ demo1/Assets/Scripts/EnemyHealth.cs
@@ -1,27 +1,43 @@
 using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
-    public Color hitColor;
-    public float lerpSpeed;
+    [Header("Enemy Health")]
     [SerializeField] private float health;
     private float currentHealth;
+    
+    [Header("Damage Feedback")]
+    Vector3 baseScale;
+    Vector3 scaleOffset;
+    public float hitShrinkAmount = 0.15f;
+    public float shrinkSpeed = 25f;
+    public float returnSpeed = 10f;
+
+    [SerializeField] private Color hitColor;
+    [SerializeField] private float hitEffectSpeed;
     private Material material;
     private Color defaultColor;
+
 
     private void Start() {
         currentHealth = health;
         material = GetComponent<MeshRenderer>().material;
         material.EnableKeyword("_EMISSION");
+        baseScale = transform.localScale;
         defaultColor = new(0, 0, 0);
     }
     private void Update()
     {
         Color current = material.GetColor("_EmissionColor");
-        Color next = Color.Lerp(current, defaultColor, Time.deltaTime * lerpSpeed);
+        Color next = Color.Lerp(current, defaultColor, Time.deltaTime * hitEffectSpeed);
+
+        scaleOffset = Vector3.Lerp(scaleOffset, Vector3.zero, Time.deltaTime * returnSpeed);
+        transform.localScale = baseScale + scaleOffset;
+
         material.SetColor("_EmissionColor", next);
     }
     public void Damage(float amount) {
         currentHealth -= amount;
+        scaleOffset = Vector3.one * -hitShrinkAmount;
         material.SetColor("_EmissionColor", hitColor * 5f);
         CheckForDeath();
     }
