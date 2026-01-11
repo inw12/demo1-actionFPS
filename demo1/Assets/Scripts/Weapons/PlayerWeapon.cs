f2367b3f775d60d920afd7bd6960523b8f6bfc30
iann <ianathanwong@gmail.com>
2026-01-11 11:46:55 -0800

    Look Sway
    Weapon will now have a "sway" effect when player looks around

--- demo1/Assets/Scripts/Weapons/PlayerWeapon.cs
+++ demo1/Assets/Scripts/Weapons/PlayerWeapon.cs
@@ -3,12 +3,32 @@ public class PlayerWeapon : MonoBehaviour
 {
     [Header("Weapon Object")]
     [SerializeField] private GameObject weapon;
-    [Header("UI Elements")]
     private Weapon currentWeapon;
     private GameObject weaponInstance;
+    [Header("Weapon Sway")]
+    public float swayAmount = 0.05f;
+    public float swaySmooth = 8f;
+    private MovementInput input;
+    private Vector3 defaultPos;
+    private Vector3 finalOffset;
 
     private void Start() {
         if (weapon) UpdateWeapon(weapon);
+        defaultPos = transform.localPosition;
+        input = GetComponentInParent<MovementInput>();
+    }
+    private void Update()
+    {
+        // Look sway
+        float x = input.defaultActions.Look.ReadValue<Vector2>().x;
+        float y = input.defaultActions.Look.ReadValue<Vector2>().y;
+        Vector3 lookOffset = new(
+            x * swayAmount,
+            y * swayAmount,
+            0
+        );
+        finalOffset = Vector3.Lerp(finalOffset, lookOffset, Time.deltaTime * swaySmooth);
+        transform.localPosition = defaultPos + finalOffset;
     }
     public void Attack(bool attackButtonDown) => currentWeapon.Attack(attackButtonDown);
     public void UpdateWeapon(GameObject newWeapon)
